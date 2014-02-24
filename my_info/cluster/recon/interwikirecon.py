9a51907323d0d62a25ae3d717db0dd49de2e7069
Mattia Larentis <mattia@larentis.eu>
2014-02-24 22:24:32 +0100

    request to interwikirecon / 2

--- my_info/cluster/recon/interwikirecon.py
+++ my_info/cluster/recon/interwikirecon.py
@@ -7,26 +7,33 @@ class InterWikiRecon(object):
         self.cache = RedisCache()
 
     def get_inter_wikilinks(self, page):
+        """
+        >>> a = InterWikiRecon()
+        >>> a.get_inter_wikilinks('http://it.wikipedia.org/wiki/Mozilla')
+        {u'en': u'http://en.wikipedia.org/wiki/Mozilla', \
+u'it': u'http://it.wikipedia.org/wiki/Mozilla'}
+        >>> a.get_inter_wikilinks('http://en.wikipedia.org/wiki/Mozilla')
+        {u'en': u'http://en.wikipedia.org/wiki/Mozilla', \
+u'it': u'http://it.wikipedia.org/wiki/Mozilla'}
+        """
         key = "{}:interwikirecon".format(page)
         if not self.cache.has(key):
-            url = 'http://interwikirecon.spaziodati.eu/reconcile?queries={' \
-                  '"q0":{"query": "' + page + '", "type": "Wikipedia %s"}}'
+
+            given_lang = 'IT' if '://it.' in page else 'EN'
+            new_lang = 'IT' if given_lang == 'EN' else 'EN'
+
+            url = 'http://interwikirecon.spaziodati.eu/reconcile?' \
+                  'queries={"q0":{"query": "%s", "type": "Wikipedia %s"}}' \
+                  '' % (page, new_lang)
 
             try:
-                en = requests.get(
-                    url.replace("%s", 'EN')).json()['q0']['result'][0]['id']
+                new_page = requests.get(url).json()['q0']['result'][0]['id']
             except IndexError:
-                en = None
-
-            try:
-                it = requests.get(
-                    url.replace("%s", 'IT')).json()['q0']['result'][0]['id']
-            except IndexError:
-                it = None
+                new_page = None
 
             self.cache.set(key, {
-                'en': en,
-                'it': it,
+                given_lang: page,
+                new_lang: new_page,
             })
 
         return self.cache.get(key)
