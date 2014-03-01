385e192721a06376ac86e845810062b7e5d8d3f6
Mattia Larentis <mattia@larentis.eu>
2014-03-01 18:55:28 +0100

    fixed celery

--- my_info/main/celery.py
+++ my_info/main/celery.py
@@ -1,14 +1,22 @@
 from __future__ import absolute_import
+import os
 
 from celery import Celery
-from my_info.settings import BROKER_URL, CELERY_RESULT_BACKEND
+from my_info import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_info.settings')
 
 app = Celery(
     'main',
-    broker=BROKER_URL,
-    backend=CELERY_RESULT_BACKEND,
+    broker=settings.BROKER_URL,
     include=['my_info.main.tasks']
 )
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
 
 if __name__ == '__main__':
     app.start()
